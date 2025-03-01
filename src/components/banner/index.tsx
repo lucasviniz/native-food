e33f4ff3951d1e423e4b0c23c816fab181ad5c08
lucasviniz <howaitosame_7@hotmail.com>
2025-03-01 16:15:41 -0300

    first commit

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,11 @@
+import { View, Pressable, Text, Image } from 'react-native';
+import PagerView from "react-native-pager-view"
+export default function Banner() {
+ return (
+   <View className='w-full h-36 rounded-2xl mt-5 mb-4'>
+        <PagerView>
+            
+        </PagerView>
+   </View>
+  );
+}
